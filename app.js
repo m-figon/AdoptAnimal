b2705f284285f91568740a40ff4279bd023065bc
mateusz <dalvan234@gmail.com>
2020-07-15 16:00:07 +0200

    adoption form divided to 2 parts

--- app.js
+++ app.js
@@ -96,95 +96,136 @@
     });
     this.get('#/:type/:id/adoption', function (context) {
       urlNavigation(context);
-      let inputValues = ["First Name", "Last Name", "City", "Street", "Phone Number", "Email"];
-      let errors = ["First name must containt from 3 to 12 a-z letters", "Last Name must containt from 3 to 12 a-z letters", "City name must be 3 or more letters long", "Street name must be 3 or more letters long", "Phone Number must contain 9 digits without space", "Please enter correct email address"];
+      let inputValues1 = ["First Name", "Last Name", "City", "Street"];
+      let errors1 = ["First name must containt from 3 to 12 a-z letters", "Last Name must containt from 3 to 12 a-z letters", "City name must be 3 or more letters long", "Street name must be 3 or more letters long"];
+      let inputValues2 = ["Phone Number", "Email"];
+      let errors2= ["Phone Number must contain 9 digits without space", "Please enter correct email address"];
       fetch('https://rocky-citadel-32862.herokuapp.com/AdoptAnimal/animals')
         .then(response => response.json())
         .then(data => {
           animals = data.slice();
+          let firstNameVal;
+          let lastNameVal;
+          let cityVal;
+          let streetVal;
           $(".loading").remove();
           $(".app").append("<div class='adoption-form'></div");
-          $(".adoption-form").append("<div class='adoption-content'></div>");
-          $(".adoption-content").append("<h2>Adoption form</h2>");
-          for (let i = 0; i < inputValues.length; i++) {
-            $(".adoption-content").append("<input value='" + inputValues[i] + "' type='text'>");
-            $(".adoption-content").append("<p id='hidden'>" + errors[i] + "</p>");
+          $(".adoption-form").append("<div class='adoption-content1'></div>");
+          $(".adoption-content1").append("<h2>Adoption form</h2>");
+          for (let i = 0; i < 4; i++) {
+            $(".adoption-content1").append("<input value='" + inputValues1[i] + "' type='text'>");
+            $(".adoption-content1").append("<p id='hidden'>" + errors1[i] + "</p>");
           }
-          $(".adoption-content").append("<input value='" + animals[context.params.id].name + "' type='text' readonly>");
-          $(".adoption-content").append("<input value='" + animals[context.params.id].type.substr(0, animals[context.params.id].type.length - 1) + "' type='text' readonly>");
-          $(".adoption-content input:lt(6)").on('focus', (e) => {
-            if (e.target.value === "First Name" || e.target.value === "Last Name" || e.target.value === "City" || e.target.value === "Street" || e.target.value === "Email" || e.target.value === "Phone Number") {
+          $(".adoption-content1 input:lt(4)").on('focus', (e) => {
+            if (e.target.value === "First Name" || e.target.value === "Last Name" || e.target.value === "City" || e.target.value === "Street") {
               e.target.value = ""
             }
           });
-          $(".adoption-content input:gt(5)").css({ 'background-color': '#b5c7a3' });
-          for (let i = 0; i < inputValues.length; i++) {
-            $(".adoption-content input:eq(" + i + ")").on('blur', (e) => {
+          for (let i = 0; i < 4; i++) {
+            $(".adoption-content1 input:eq(" + i + ")").on('blur', (e) => {
               if (e.target.value === "" || e.target.value === " ") {
-                e.target.value = inputValues[i];
+                e.target.value = inputValues1[i];
               }
             });
           }
-          $(".adoption-content").append("<button>ADOPT</button>");
-          $(".adoption-content button").on('click', () => {
-            let correctFlag = true;
-            let matchingValues = [/^[a-zA-Z0-9\.\-_]{3,12}$/, /^[a-zA-Z0-9\.\-_]{3,12}$/, /^[Z0-9]{9}$/, /^[a-z0-9\._\-]+@[a-z0-9\.\-]+\.[a-z]{2,4}$/];
+          $(".adoption-content1").append("<button>NEXT</button>");
+          let correctFlag1 = true;
+          let correctFlag2 = true;
+          $(".adoption-content1 button").on('click', () => {
+            correctFlag1 = true;
+            let matchingValues = [/^[a-zA-Z0-9\.\-_]{3,12}$/, /^[a-zA-Z0-9\.\-_]{3,12}$/];
             let values = ["City", "Street"];
             for (let i = 0; i < 2; i++) {
-              if ($(".adoption-content input:eq(" + i + ")")[0].value.match(matchingValues[i]) === null) {
-                $(".adoption-content p:eq(" + i + ")").attr('id', '');
-                correctFlag = false;
+              if ($(".adoption-content1 input:eq(" + i + ")")[0].value.match(matchingValues[i]) === null) {
+                $(".adoption-content1 p:eq(" + i + ")").attr('id', '');
+                correctFlag1 = false;
               } else {
-                $(".adoption-content p:eq(" + i + ")").attr('id', 'hidden');
+                $(".adoption-content1 p:eq(" + i + ")").attr('id', 'hidden');
               }
             }
             for (let i = 2; i < 4; i++) {
-              if (($(".adoption-content input:eq(" + i + ")")[0].value.length < 3) || $(".adoption-content input:eq(" + i + ")")[0].value === values[i - 2]) {
-                $(".adoption-content p:eq(" + i + ")").attr('id', '');
-                correctFlag = false;
+              if (($(".adoption-content1 input:eq(" + i + ")")[0].value.length < 3) || $(".adoption-content1 input:eq(" + i + ")")[0].value === values[i - 2]) {
+                $(".adoption-content1 p:eq(" + i + ")").attr('id', '');
+                correctFlag1 = false;
               } else {
-                $(".adoption-content p:eq(" + i + ")").attr('id', 'hidden');
-              }
-            }
-            for (let i = 2; i < 4; i++) {
-              if ($(".adoption-content input:eq(" + (i + 2) + ")")[0].value.match(matchingValues[i]) === null) {
-                $(".adoption-content p:eq(" + (i + 2) + ")").attr('id', '');
-                correctFlag = false;
-              } else {
-                $(".adoption-content p:eq(" + (i + 2) + ")").attr('id', 'hidden');
+                $(".adoption-content1 p:eq(" + i + ")").attr('id', 'hidden');
               }
             }
 
-            if (correctFlag) {
-              fetch('https://rocky-citadel-32862.herokuapp.com/AdoptAnimal/adoptions', {
-                method: 'POST',
-                body: JSON.stringify({
-                  firstName: $(".adoption-content input:eq(0)")[0].value,
-                  lastName: $(".adoption-content input:eq(1)")[0].value,
-                  city: $(".adoption-content input:eq(2)")[0].value,
-                  street: $(".adoption-content input:eq(3)")[0].value,
-                  phoneNumber: $(".adoption-content input:eq(4)")[0].value,
-                  email: $(".adoption-content input:eq(5)")[0].value,
-                  animalName: $(".adoption-content input:eq(6)")[0].value,
-                  animalType: $(".adoption-content input:eq(7)")[0].value
-                }),
-                headers: {
-                  "Content-type": "application/json; charset=UTF-8"
-                }
-              }).then(data => {
-                console.log(data);
-                let info = ['<h2>Adoption request sent!</h2>', '<h1>We will respond to you soon</h1>', '<h1>You did something great today</h1>']
-                alert('adoption request sent');
-                $(".adoption-form").remove();
-                $(".app").append("<div class='info'></div>");
-                $(".info").append("<div class='info-details'></div>");
-                for (let item of info) {
-                  $(".info-details").append(item);
+            if (correctFlag1) {
+              firstNameVal = $(".adoption-content1 input:eq(0)")[0].value;
+              lastNameVal = $(".adoption-content1 input:eq(1)")[0].value;
+              cityVal = $(".adoption-content1 input:eq(2)")[0].value;
+              streetVal = $(".adoption-content1 input:eq(3)")[0].value;
+              $(".adoption-content1").remove();
+              $(".adoption-form").append("<div class='adoption-content2'></div");
+              $(".adoption-content2").append("<h2>Adoption form</h2>");
+              for (let i = 0; i < inputValues2.length; i++) {
+                $(".adoption-content2").append("<input value='" + inputValues2[i] + "' type='text'>");
+                $(".adoption-content2").append("<p id='hidden'>" + errors2[i] + "</p>");
+              }
+              $(".adoption-content2").append("<input value='" + animals[context.params.id].name + "' type='text' readonly>");
+              $(".adoption-content2").append("<input value='" + animals[context.params.id].type.substr(0, animals[context.params.id].type.length - 1) + "' type='text' readonly>");
+
+              $(".adoption-content2 input:lt(2)").on('focus', (e) => {
+                if (e.target.value === "Email" || e.target.value === "Phone Number") {
+                  e.target.value = ""
                 }
               });
+              $(".adoption-content2 input:gt(1)").css({ 'background-color': '#b5c7a3' });
+              for (let i = 0; i < 2; i++) {
+                $(".adoption-content2 input:eq(" + i + ")").on('blur', (e) => {
+                  if (e.target.value === "" || e.target.value === " ") {
+                    e.target.value = inputValues2[i];
+                  }
+                });
+              }
+              $(".adoption-content2").append("<button id='adopt'>ADOPT</button>");
+              $(".adoption-content2 button").on('click', () => {
+                console.log('adopt');
+                correctFlag2 = true;
+                let matchingValues2 = [/^[Z0-9]{9}$/, /^[a-z0-9\._\-]+@[a-z0-9\.\-]+\.[a-z]{2,4}$/];
+                for (let i = 0; i < matchingValues2.length; i++) {
+                  if ($(".adoption-content2 input:eq(" + (i) + ")")[0].value.match(matchingValues2[i]) === null) {
+                    $(".adoption-content2 p:eq(" + (i) + ")").attr('id', '');
+                    correctFlag2 = false;
+                  } else {
+                    $(".adoption-content2 p:eq(" + (i) + ")").attr('id', 'hidden');
+                  }
+                }
+                if (correctFlag1 && correctFlag2) {
+                  fetch('https://rocky-citadel-32862.herokuapp.com/AdoptAnimal/adoptions', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                      firstName: firstNameVal,
+                      lastName: lastNameVal,
+                      city: cityVal,
+                      street: streetVal,
+                      phoneNumber: $(".adoption-content2 input:eq(0)")[0].value,
+                      email: $(".adoption-content2 input:eq(1)")[0].value,
+                      animalName: $(".adoption-content2 input:eq(2)")[0].value,
+                      animalType: $(".adoption-content2 input:eq(3)")[0].value
+                    }),
+                    headers: {
+                      "Content-type": "application/json; charset=UTF-8"
+                    }
+                  }).then(data => {
+                    console.log(data);
+                    let info = ['<h2>Adoption request sent!</h2>', '<h1>We will respond to you soon</h1>', '<h1>You did something great today</h1>']
+                    alert('adoption request sent');
+                    $(".adoption-form").remove();
+                    $(".app").append("<div class='info'></div>");
+                    $(".info").append("<div class='info-details'></div>");
+                    for (let item of info) {
+                      $(".info-details").append(item);
+                    }
+                  });
+                }
+              })
             }
           })
-        })
+          
+        });
     });
   });
 
